d4da9f1ea67c93997e5fd1b9c97b0e18652cba13
马路 <windy005@126.com>
2023-08-22 20:03:40 +0800

    project initialization

---
+++ src/main/java/com/example/webfluxsandbox/WebFluxSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxSandboxApplication.class, args);
+    }
+
+}
