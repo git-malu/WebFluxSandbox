17904fa7dc2755ef6df4a149cc000bf7c15fdd04
马路 <windy005@126.com>
2023-08-23 22:02:11 +0800

    this one also works

--- src/main/java/com/example/webfluxsandbox/config/LoggingConfig.java
+++ src/main/java/com/example/webfluxsandbox/config/LoggingConfig.java
@@ -1,26 +1,25 @@
 package com.example.webfluxsandbox.config;
 
+import reactor.core.publisher.Mono;
+
 import org.slf4j.MDC;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
-@Configuration
-public class LoggingConfig {
+@Component
+class LoggingWebFilter implements WebFilter {
 
-    @Bean
-    public WebFilter logRequest() {
-        return (ServerWebExchange exchange, WebFilterChain chain) -> {
-            // Get the request id from the X-Request-ID header
-            String requestId = exchange.getRequest().getHeaders().getFirst("X-Request-ID");
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        // Get the request id from the X-Request-ID header
+        String requestId = exchange.getRequest().getHeaders().getFirst("X-Request-Id");
 
-            // Put the request id into the MDC
-            MDC.put("requestId", requestId);
+        // Put the request id into the MDC
+        MDC.put("X-Request-Id", requestId);
 
-            // Make sure to remove the MDC variable when the request is finished
-            return chain.filter(exchange).doFinally(sig -> MDC.remove("requestId"));
-        };
+        // Make sure to remove the MDC variable when the request is finished
+        return chain.filter(exchange).doFinally(sig -> MDC.remove("X-Request-Id"));
     }
 }
--- src/main/java/com/example/webfluxsandbox/controller/HelloWorldController.java
+++ src/main/java/com/example/webfluxsandbox/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.webfluxsandbox.controller;
 
 // rest controller
 
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld() {
+        System.out.println(MDC.get("X-Request-Id"));
         return "Hello, World!";
     }
 }
